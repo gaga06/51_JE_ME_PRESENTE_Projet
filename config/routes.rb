0e2d31468427dac787f659d7ccb2b02dc0204d1a
Gaelle <gaelle.gorgori@gmail.com>
2018-08-01 00:41:41 +0200

    Ajout de liens page hom

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/home', to: 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about', as: 'index'
-  get '/about/me', to: 'static_pages#about_me', as: 'about/me'
-  get '/about/groupe', to: 'static_pages#about_groupe', as: 'about/groupe'
+  get '/about/me', to: 'static_pages#about_me', as: 'me'
+  get '/about/groupe', to: 'static_pages#about_groupe', as: 'groupe'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
